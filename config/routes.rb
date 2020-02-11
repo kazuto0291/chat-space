444bffd6124fd7ce46c9bf46f30b73c2cd0c0fd7
kazuto0291 <zutozuto03170291@gmail.com>
2020-02-11 15:46:06 +0900

    model-create-association

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
-  resourcds :groups, only:[:new, :create]
+  resources :groups, only: [:new, :create]
 end
