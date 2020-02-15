42594337ffbd3610d953d01475f1548af559a3d8
kazuto0291 <zutozuto03170291@gmail.com>
2020-02-15 17:14:54 +0900

    ajax-json-controller

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,16 @@
 $(function(){
   $('#new_message').on('submit', function(e) {
-    console.log("gaga");
-    e.preventDefault()
+    e.preventDefault();
+    var formDate = new FormDate(this);  //thisはform
+    var url = $(this).attr(action);      //thisからactionのパスを取得し代入(/groups/:id番号/messages)
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formDate,
+      dataType: 'json',
+      processDate: false,
+      contentType: false
+    })
   });
 });
