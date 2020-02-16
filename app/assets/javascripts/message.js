a087b78cd9332188bbb5afb93a6573c4f1034cb6
kazuto0291 <zutozuto03170291@gmail.com>
2020-02-16 17:09:19 +0900

    html-append

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,62 @@
 $(function(){
-  $('#new_message').on('submit', function(e) {
-    e.preventDefault();
-    var formDate = new FormDate(this);  //thisはform
-    var url = $(this).attr(action);      //thisからactionのパスを取得し代入(/groups/:id番号/messages)
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message__box">
+            <div class="message__box__name">
+              ${message.user_mame}
+            </div>
+            <div class="message__box__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="massage__text">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message__box">
+            <div class="message__box__name">
+              ${message.user_mame}
+            </div>
+          <div class="message__box__date">
+            ${message.created_at}
+          </div>
+          </div>
+          <div class="lower-message">
+            <p class="massage__text">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
 
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
-      type: 'POST',
-      data: formDate,
+      type: "POST",
+      data: formData,
       dataType: 'json',
-      processDate: false,
+      processData: false,
       contentType: false
     })
-  });
+
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.main-contents').append(html)
+      
+    })
+  })
 });
