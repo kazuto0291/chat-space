c2beb4d6d5873b2b5e8e76b70b24fc47728a3739
kazuto0291 <zutozuto03170291@gmail.com>
2020-02-17 15:26:32 +0900

    非同期通信の結果のhtml表示

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,23 @@
 $(function() {
+  function addUser(user) {
+    let html =`
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+      `;
+    $("#user-search-result").append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+      `;
+      $("#user-search-result").append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
 
@@ -10,10 +29,21 @@ $(function() {
 
     })
     .done(function(users) {
-      console.log("成功");
+      $("#user-seach-result").empty();
+        console.log(users)
+      if (users.lenght !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.lenght == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
     })
     .fail(function() {
+      // alert("通信エラーです。ユーザーが表示できません。");
       console.log("失敗");
-    })
+    });
   });
 });
